0c493f1e6f71002c68f9a6e74998bdfb0ff369cf
Devendar Singh Rawat <devendasinghrawat925@gmail.com>
2025-01-27 11:35:40 +0530

    Merge Two SortedList

--- LinkedList/MergeTwoSortedList.java
+++ LinkedList/MergeTwoSortedList.java
@@ -1,12 +1,27 @@
 
 class Solution {
-    public ListNode middleNode(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast!=null&&fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head2=new ListNode(-1);
+        ListNode temp=head2;
+        ListNode t1=list1;
+        ListNode t2=list2;
+        while(t1!=null&&t2!=null){
+            if(t1.val<=t2.val){
+           
+                temp.next=t1;
+                t1=t1.next;
+                temp=temp.next;
+            }
+            else{
+
+             temp.next=t2;
+             t2=t2.next;
+             temp=temp.next;
+            }
         }
-        return slow;
+        if(t1!=null) temp.next=t1;
+        if(t2!=null) temp.next=t2;
+        return head2.next;
+       
     }
 }
