929669e3d86736d9318804778d1cb21dd7ac9cdb
Devendar Singh Rawat <devendasinghrawat925@gmail.com>
2025-02-05 23:40:59 +0530

    Reverse pairs

---
+++ Array/ReversePairs.java
@@ -0,0 +1,62 @@
+class Solution {
+    private void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); 
+        int count=0;
+        int left = low;     
+        int right = mid + 1;  
+
+     
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+           
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+     
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+    
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+    public int helper(int nums[],int st,int mid,int end){
+        int count=0;
+        int right=mid+1;
+        for(int i=st;i<=mid;i++){
+         while(right<=end&&nums[i]>(long)2*nums[right]) right++;
+         count+=right-(mid+1);
+        }
+        return count;
+    }
+
+    public int mergeSort(int[] arr, int low, int high) {
+             int count=0;
+        if (low >= high) return count;
+  
+        int mid = (low + high) / 2 ;
+        count+=mergeSort(arr, low, mid);  
+       count+=mergeSort(arr, mid + 1, high);
+       count+=helper(arr,low,mid,high); 
+        merge(arr, low, mid, high);
+       return count;
+    }
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums,0,nums.length-1);
+    }
+}
